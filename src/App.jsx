0a9b5e51cc5640fc113c97212217946ae2853500
nitinsharma1845 <nitinsharma992740@gmail.com>
2025-06-18 21:07:29 +0530

    Appwrite authorization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const App = () => {
-  return (
-    <div className='text-3xl'>App</div>
-  )
-}
+  return <div className="text-3xl">App</div>;
+};
 
-export default App
+export default App;
